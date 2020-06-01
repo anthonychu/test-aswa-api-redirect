0a1e125a9df2a33a5e5d2cc611361c2dd605c9a4
Anthony Chu <anthony@anthonychu.ca>
2020-06-01 20:36:37 +0000

    Add second route

--- api/redirectabsolute/index.js
+++ api/redirectabsolute/index.js
@@ -2,7 +2,7 @@ module.exports = async function (context, req) {
     context.res = {
         status: 302,
         headers: {
-            "location": "/thankyou.html"
+            "location": "https://delightful-island-0d63ac010.azurestaticapps.net/thankyou.html"
         }
     };
 };
